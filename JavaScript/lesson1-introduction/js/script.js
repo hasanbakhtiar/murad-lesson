de52efff48204e33538aa9b28567e621523966b2
Hasan Bakhtiar <hasan.doctype@gmail.com>
2025-06-18 12:22:49 +0400

    first lesson

---
+++ JavaScript/lesson1-introduction/js/script.js
@@ -0,0 +1,127 @@
+// ECMAScript => European Computer Manufacturers Association
+// debugger;
+// console.log("Hello1");
+// console.log("Hello2");
+// console.log("Hello3");
+// console.log("Hello4");
+// console.log("Hello5");
+// console.log("Hello6");
+// console.log("Hello7");
+
+// Primitive DT - string | number | boolean
+
+// string
+// var info = "Hello";
+// console.log(info);
+
+//number
+// var info = "5";
+// var infoTwo = 5;
+// console.log(info+infoTwo);
+
+// boolean
+// var info = true;
+// console.log(info)
+
+//Composite DT - array | object
+// var infoArr = ["Hello",5,true];
+// console.log(infoArr);
+
+// var infoObj = {
+//   title:"Asus ROG",
+//   price:3000,
+//   newDevice:true
+// }
+// console.log(infoObj)
+
+//Special DT - unbdefined | null
+
+// var info = null;
+// console.log(info);
+
+// OPERATORS
+
+// assignment operators
+// // =
+// // +=
+// // -=
+// // *=
+// // /=
+// // %=
+// // **=
+//
+// var a = 5;
+// a **= 10; //a = a +10
+// console.log(a);
+
+// // arithmetic operators
+// // +
+// // -
+// // *
+// // /
+// // %
+// // **
+// // ++
+// // --
+
+// var a = 5;
+// var b = 10;
+// a--;
+// console.log(a)
+
+// // comparison operators
+// // ==
+// // ===
+// // !=
+// // !==
+// // <
+// // <=
+// // >
+// // >=
+// if ("a"<"b") {
+//   console.log('this is true');
+// } else {
+//   console.log('this is false');
+// }
+
+// // logical operators
+// // && - AND
+// // || - OR
+// // !  - NOT
+
+// if (!((5<10 || false) && (100=="100"&&false))&&true) {
+//   console.log(true)
+// }else{
+//   console.log(false)
+// }
+
+// // --------------------------------------------------------------------------------------------------
+// // Other (Special) Operators (Xüsusi operatorlar) Start
+//
+// // ,  (vergül)(kodları bir-birindən ayırmaq və sintaksis qanun uyğunluqları icra etmək üçündür )
+// var a = 5,  b= 10
+// console.log(a,b);
+//
+//
+// // \            (tərs slash)(Qaçındırma operatorudur)
+// var info = "JavaScript\"s Book";
+// console.log(info);
+//
+// // typeof       (tip üçün)(datanın tipini təyin etmək üçündür)
+//
+//
+// var a = true;
+// var b = typeof a;
+// console.log(b);
+//
+//
+// // void         (dəyişkənin dəyərini undefined etmək üçündür)
+//
+// var a = 5;
+// console.log(void(a));
+
+// var info = prompt("Enter name");
+// console.log(info);
+
+var info = confirm('Hello');
+console.log(info);
